b4329ca09fa20fc4a4bc10e6d3a4f248fc832fe1
nn12vl9l <nn12vl9lsoft@gmail.com>
2021-10-31 16:15:19 +0900

    Initial Commit

---
+++ ListViewApp/ListViewAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListViewAppApp.swift
+//  ListViewApp
+//
+//  Created by 小倉瑞希 on 2021/10/31.
+//
+
+import SwiftUI
+
+@main
+struct ListViewAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
